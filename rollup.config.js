70d24a64285ba055746b0a67186eb5aeda421c2a
Niek Saarberg <n.saarberg@gmail.com>
2019-03-17 12:40:57 +0100

    Project init

---
+++ rollup.config.js
@@ -0,0 +1,40 @@
+import resolve from 'rollup-plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript2';
+import commonjs from 'rollup-plugin-commonjs'
+
+import pkg from './package.json';
+
+const banner = `/**
+  * ${pkg.name} v${pkg.version}
+  * (C) 2019-${new Date().getFullYear()} ${pkg.author}
+  * Released under the ${pkg.license} License.
+  */`;
+
+  export default [
+    {
+      input: 'src/index.ts',
+      external: [
+        ...Object.keys(pkg.peerDependencies || {}),
+      ],
+      output: [
+        {
+          banner,
+          file: pkg.main,
+          format: 'cjs',
+          exports: 'named',
+        },
+        {
+          banner,
+          file: pkg.module,
+          format: 'es'
+        }
+      ],
+      plugins: [
+        resolve({
+          extensions: ['.ts', '.tsx'],
+        }),
+        typescript(),
+        commonjs(),
+      ]
+    }
+  ];
---
+++ wallaby.js
@@ -0,0 +1,22 @@
+module.exports = wallaby => ({
+  files: [
+    'tsconfig.json',
+    'src/**/*.ts?(x)',
+    'src/**/*.snap',
+    '!src/**/*.spec.ts?(x)',
+  ],
+  tests: [
+    'src/**/*.spec.ts?(x)',
+  ],
+  env: {
+    type: 'node',
+    runner: 'node',
+  },
+  compilers: {
+    '**/*.ts?(x)': wallaby.compilers.typeScript({
+      isolatedModules: true,
+      module: 'commonjs',
+    }),
+  },
+  testFramework: 'jest',
+});
